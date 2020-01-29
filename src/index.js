921d5cfb106cc216a9db732c5625a633fab5882f
Tijesunimi <amesinlolaprecious@gmail.com>
2020-01-29 22:49:31 +0100

    Setup mongoose to connect to db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 import app from './app';
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
