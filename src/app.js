3daa9c01ce145dad90e42cdf1eaa0d76bec64800
Tijesunimi <amesinlolaprecious@gmail.com>
2020-01-29 22:35:54 +0100

    Setup basic express API server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+// Default route
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to Twitter API v1" });
+});
+
+export default app;
